21a0de7a2df252dbe1adafcffaa12a5559bd8dc6
gianvalen <157687026+gianvalen@users.noreply.github.com>
2024-03-18 16:37:53 +0800

    Edited tasks

--- mysite/tasks/views.py
+++ mysite/tasks/views.py
@@ -1,10 +1,11 @@
+from typing import Any
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Task
+from .models import Task, TaskGroup
 
 
 def index(request):
@@ -12,8 +13,18 @@ def index(request):
 
 def task_list(request):
     tasks = Task.objects.all()
-    ctx = { "tasks": tasks }
-
+    ctx = { 
+        "object_list": tasks,
+        "taskgroups": TaskGroup.objects.all()
+    }
+    if request.method == "POST":
+        task = Task()
+        task.name = request.POST['task_name']
+        task.due_date = request.POST.get('task_due')
+        task.taskgroup = TaskGroup.objects.get(
+            pk=request.POST.get('taskgroup')
+        )
+        task.save()
     return render(request, 'task_list.html', ctx)
 
 def task_detail(request, pk):
@@ -26,6 +37,21 @@ def task_detail(request, pk):
 class TaskListView(ListView):
     model = Task
     template_name = 'task_list.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['taskgroups'] = TaskGroup.objects.all()
+        return ctx
+
+    def post(self, request, *args, **kwargs):
+        task = Task()
+        task.name = request.POST['task_name']
+        task.due_date = request.POST.get('task_due')
+        task.taskgroup = TaskGroup.objects.get(
+            pk=request.POST.get('taskgroup')
+        )
+        task.save()
+        return self.get(request, *args, **kwargs)
 
 
 class TaskDetailView(LoginRequiredMixin, DetailView):
